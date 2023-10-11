d4a0aed954f1138f9b7c70757e5a9bfef409aff8
Azwad Islam <azwad.sachcha@gmail.com>
2023-10-12 00:54:17 +0600

    Create 1015.cpp

---
+++ 1015.cpp
@@ -0,0 +1,18 @@
+//1015
+
+#include <iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main(){
+double x1,y1,x2,y2,dis;
+cin>>x1>>y1;
+cin>>x2>>y2;
+dis = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+cout<<fixed;
+cout<<setprecision(4)<<dis<<endl;
+
+
+return 0;
+
+}
